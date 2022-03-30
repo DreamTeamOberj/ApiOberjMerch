7edde68408310eda2dbb6e4df385e7f88c776c7c
Romain857 <romainmart.85@gmail.com>
2022-03-30 11:43:57 +0200

    crud

--- app/server/routes/users.py
+++ app/server/routes/users.py
@@ -5,10 +5,26 @@ users = APIRouter()
 
 
 @users.get("/users")
-def read_all_users(product_id: int):
-    return {"product_id": product_id}
+async def read_all_users(users_id: int):
+    return {"users_id": users_id}
 
 
 @users.get("/users/{users_id}")
-def read_users(users_id: int):
+async def read_users(users_id: int):
+    return {"users_id": users_id}
+
+@users.post("/users/")
+async def create_user(users_id: int):
+    return {"users_id": users_id}
+
+@users.put("/users/{users_id}")
+async def update_users(users_id: int):
+    return {"users_id": users_id}
+
+@users.patch("/users/{users_id}")
+async def patch_users(users_id: int):
+    return {"users_id": users_id}
+
+@users.delete("/users/{users_id}")
+async def delete_users(users_id: int):
     return {"users_id": users_id}
