0411869e87e8374987b860630b9720dbddc5d7f7
Antoine <a.deyris85@gmail.com>
2022-03-30 10:02:07 +0200

    [ADD] Product Model

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/product")
+def read_all_products(product_id: int, q: Optional[str] = None):
+    return {"product_id": product_id, "q": q}
+
+@app.get("/product/{product_id}")
+def read_product(product_id: int, q: Optional[str] = None):
+    return {"product_id": product_id, "q": q}
