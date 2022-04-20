0d50ecd11d9b6b39de5b5d2b84f4ada48b3906fb
Romain857 <romainmart.85@gmail.com>
2022-04-20 09:37:21 +0200

    base crud

--- app/server/routes/products.py
+++ app/server/routes/products.py
@@ -13,17 +13,21 @@ def read_all_products(product_id: int):
 def read_product(product_id: int):
     return {"product_id": product_id}
 
+
 @products.post("/product/")
 async def create_product(product_id: int):
     return {"product_id": product_id}
+
 
 @products.put("/product/{product_id}")
 async def update_product(product_id: int):
     return {"product_id": product_id}
 
+
 @products.patch("/product/{product_id}")
 async def patch_product(product_id: int):
     return {"product_id": product_id}
+
 
 @products.delete("/product/{product_id}")
 async def delete_product(product_id: int):
--- app/server/routes/users.py
+++ app/server/routes/users.py
@@ -13,17 +13,21 @@ async def read_all_users(users_id: int):
 async def read_users(users_id: int):
     return {"users_id": users_id}
 
+
 @users.post("/users/")
 async def create_user(users_id: int):
     return {"users_id": users_id}
+
 
 @users.put("/users/{users_id}")
 async def update_users(users_id: int):
     return {"users_id": users_id}
 
+
 @users.patch("/users/{users_id}")
 async def patch_users(users_id: int):
     return {"users_id": users_id}
+
 
 @users.delete("/users/{users_id}")
 async def delete_users(users_id: int):
