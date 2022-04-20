0caf9f51678ca18c9e2995c82033170e93a46acb
gabriel <gabriel.filiot@epsi.fr>
2022-04-20 14:48:37 +0200

    mongodb router

--- app/server/routes/products.py
+++ app/server/routes/products.py
@@ -23,3 +23,47 @@ async def add_product_data(product: ProductSchema = Body(...)):
     product = jsonable_encoder(product)
     new_product = await add_product(product)
     return ResponseModel(new_product, "Product added successfully.")
+
+
+@productRouter.get("/products", response_description="Products retrieved")
+async def get_students():
+    products = await retrieve_products()
+    if products:
+        return ResponseModel(products, "Products data retrieved successfully")
+    return ResponseModel(products, "Empty list returned")
+
+
+@productRouter.get("/product/{id}", response_description="Student data retrieved")
+async def get_product_data(id):
+    product = await retrieve_product(id)
+    if product:
+        return ResponseModel(product, "Product data retrieved successfully")
+    return ErrorResponseModel("An error occurred.", 404, "Product doesn't exist.")
+
+
+@productRouter.put("/product/{id}")
+async def update_product_data(id: str, req: UpdateProductModel = Body(...)):
+    req = {k: v for k, v in req.dict().items() if v is not None}
+    updated_product = await update_product(id, req)
+    if updated_product:
+        return ResponseModel(
+            "Product with ID: {} name update is successful".format(id),
+            "Product name updated successfully",
+        )
+    return ErrorResponseModel(
+        "An error occurred",
+        404,
+        "There was an error updating the user data.",
+    )
+
+
+@productRouter.delete("/product/{id}", response_description="Product data deleted from the database")
+async def delete_product_data(id: str):
+    deleted_product = await delete_product(id)
+    if deleted_product:
+        return ResponseModel(
+            "Product with ID: {} removed".format(id), "Product deleted successfully"
+        )
+    return ErrorResponseModel(
+        "An error occurred", 404, "Product with id {0} doesn't exist".format(id)
+    )
--- app/server/routes/users.py
+++ app/server/routes/users.py
@@ -23,3 +23,47 @@ async def add_user_data(user: UserSchema = Body(...)):
     user = jsonable_encoder(user)
     new_user = await add_user(user)
     return ResponseModel(new_user, "User added successfully.")
+
+
+@userRouter.get("/users", response_description="Products retrieved")
+async def get_students():
+    users = await retrieve_users()
+    if users:
+        return ResponseModel(users, "Users data retrieved successfully")
+    return ResponseModel(users, "Empty list returned")
+
+
+@userRouter.get("/user/{id}", response_description="User data retrieved")
+async def get_user_data(id):
+    user = await retrieve_user(id)
+    if user:
+        return ResponseModel(user, "User data retrieved successfully")
+    return ErrorResponseModel("An error occurred.", 404, "User doesn't exist.")
+
+
+@userRouter.put("/user/{id}")
+async def update_user_data(id: str, req: UpdateUserModel = Body(...)):
+    req = {k: v for k, v in req.dict().items() if v is not None}
+    updated_user = await update_user(id, req)
+    if updated_user:
+        return ResponseModel(
+            "User with ID: {} name update is successful".format(id),
+            "User name updated successfully",
+        )
+    return ErrorResponseModel(
+        "An error occurred",
+        404,
+        "There was an error updating the user data.",
+    )
+
+
+@userRouter.delete("/user/{id}", response_description="User data deleted from the database")
+async def delete_product_data(id: str):
+    deleted_user = await delete_user(id)
+    if deleted_user:
+        return ResponseModel(
+            "User with ID: {} removed".format(id), "User deleted successfully"
+        )
+    return ErrorResponseModel(
+        "An error occurred", 404, "User with id {0} doesn't exist".format(id)
+    )
